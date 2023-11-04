281aa836d461cf67be4f8ef8cbd4d3b68640406b
Babyhulk89 <131919459+Babyhulk89@users.noreply.github.com>
2023-11-04 02:46:25 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+// Declare the variable returnFirstTwoDrivers with const and assign an anonymous function to it.
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Declare a variable with const that is assigned an anonymous function.
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // This is an array containing two elements: the two functions that we previously defined.
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2); // This will double the fare.
+  const fareTripler = createFareMultiplier(3); // This will triple the fare.
+  const selectDifferentDrivers = function(drivers, selectFunction) {
+    return selectFunction(drivers);
+  };
